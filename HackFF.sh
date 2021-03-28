68ec2f0b0c8539012d8f256b75f81a46432467fc
aldiansyah329 <81497102+aldiansyah329@users.noreply.github.com>
2021-03-28 08:33:32 +0700

    Add files via upload

---
+++ HackFF.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+clear
+r="\033[1;31m" # merah
+g="\033[1;32m" # hijau
+y="\033[1;33m" # kuning
+b="\033[1;34m" # biru
+p="\033[1;35m" # ungu
+cy="\033[1;36m" # biru muda
+w="\033[1;37m" # putih
+
+echo $cy
+figlet "SCRIPT HACK AKUN FF"
+echo
+sleep 4
+
+clear
+
+echo $y
+figlet "HACK FF"
+echo $w"///////////////////////////////////////////"
+echo $cy" Author"$r"  : "$cy"BANGSAT GANS"
+echo $cy" pembuat"$r" : "$cy"ALDIANSYAH"
+echo $cy" Team"$r"    : "$cy"Cyber Muslim Army"
+echo $cy" youtube channel": $cy"Aldiansyah [aldi]"
+echo $w"///////////////////////////////////////////"
+        sleep 3
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo    
+read -p " MASUKAN ID AKUN FREE FIRE KAMU : " pil;
+echo
+echo
+echo $g"SELAMAT AKUN TELAH ANDA HACK"
+echo $r"-----------------------------------------------------"
+echo $cy"no hp :"$g"085727493648"
+echo $cy"kata sandi :"$g"sayang"
+echo $r"-----------------------------------------------------"
